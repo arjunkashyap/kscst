de874539ab4f2d80cb95051f0568859051d9e3b7
Arjuna <arjun.kashyap@yahoo.co.in>
2014-08-18 15:22:53 +0530

    Commit No. 2 - Initialization of design

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Home | KSCST | Student Project Program</title>
+    <meta name="description" content="Student Project Programme, KSCST">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="php/style/normalize.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="php/style/indexstyle.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="php/style/font-awesome-4.1.0/css/font-awesome.min.css" media="all" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript" src="php/js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="php/js/jquery-ui.min.js"></script>
+</head>
+
+<body>
+    <header class="app-bar">
+        <div class="htitle short"><img src="php/images/logo.png" alt="KSCST Logo"/> <p>KSCST</p></div>
+        <div class="htitle long"><img src="php/images/logo.png" alt="KSCST Logo"/> <p>Karnataka State Council for Science and Technology</p></div>
+        <nav class="navdrawer-container">
+            <ul>
+                <li><span class="typ-a"><a href="">Home</a></span></li>
+                <li><span class="typ-a"><a href="">About SPP</a></span></li>
+                <li><span class="typ-a"><a href="">Archive</a></span></li>
+                <li><span class="typ-a"><a href="">Contact</a></span></li>
+            </ul>
+        </nav>
+        <button class="menu">☰</button>        
+    </header>    
+    <div class="mainpage">
+        <div id="row1" class="container">
+            <div class="box w-300">
+                <div class="biggest clr1 pall"><p>Student<br />Project<br />Programme</p></div>
+            </div>
+            <div class="w-600">
+                <div class="box w-300">
+                    <div class="row">
+                        <div class="circle bgclr1">7200</div>
+                        <div class="desc">Projects funded</div>
+                    </div>
+                    <div class="row">
+                        <div class="circle bgclr2">21600</div>
+                        <div class="desc">Students benefited</div>
+                    </div>
+                </div>
+                <div class="box w-300">
+                    <div class="row">
+                        <div class="circle bgclr4">234</div>
+                        <div class="desc">Colleges participated</div>
+                    </div>
+                    <div class="row">
+                        <div class="circle bgclr3">37</div>
+                        <div class="desc">Years</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="row2" class="container">
+            <div class="box w-full w-50pad">
+                <p class="gText bquote">Motivated by the desire to build a strong base for practical problem solving in several engineering colleges as well as to build their R & D potential, KSCST decided to promote student project programmes in a large number of educational institutions, located in Karnataka, in areas of engineering, medicine, fisheries and agriculture. It saw an enormous reservoir of talent and creativity in the students and the faculty of engineering colleges in Karnataka. KSCST, therefore, ventured into launching and implementing a programme, the Student Project Programme for providing financial and academic support for engineering projects.</p>
+            </div>
+        </div>
+        <div id="row3" class="container">
+            <p class="big clr2 tcenter gapBelow">Find a project</p>
+            <div class="box w-full tcenter">
+                <div class="icon"><a href="#"><p><i class="fa fa-calendar"></i></p><span class="small">Year / Series</span></a></div>
+                <div class="icon"><a href="#"><p><i class="fa fa-university"></i></p><span class="small">College</span></a></div>
+                <div class="icon"><a href="#"><p><i class="fa fa-sitemap"></i></p><span class="small">Department</span></a></div>
+                <div class="icon"><a href="#"><p><i class="fa fa-users"></i></p><span class="small">Team</span></a></div>
+                <div class="icon"><a href="#"><p><i class="fa fa-search"></i></p><span class="small">Search</span></a></div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <p>&copy;2014 All rights reserved</p>
+        <p>
+                        ಕರ್ನಾಟಕ ರಾಜ್ಯ ವಿಜ್ಞಾನ ಮತ್ತು ತಂತ್ರವಿದ್ಯಾ ಮಂಡಳಿ<br />            
+            Karnataka State Council for<br />Science and Technology<br />
+            Indian Institute of Science Campus<br />
+            Bangalore - 560 012            
+        </p>
+        <p>
+            <i class="fa fa-phone" title="Phone"></i> +91 80 2334 1652/8848/8849<br />
+            <i class="fa fa-phone" title="Fax"></i> +91 80 2334 8840<br />
+            <i class="fa fa-envelope-o" title="Email"></i> office@kscst.org.in<br />
+            <i class="fa fa-envelope-o" title="Email"></i> office@kscst.iisc.ernet.in
+        </p>
+    </footer>
+<script>
+    var navdrawerContainer = document.querySelector('.navdrawer-container');
+    var appbarElement = document.querySelector('.app-bar');
+    var menuBtn = document.querySelector('.menu');
+
+    menuBtn.addEventListener('click', function() {
+        var isOpen = navdrawerContainer.classList.contains('open');
+        if(isOpen) {
+            appbarElement.classList.remove('open');
+            navdrawerContainer.classList.remove('open');
+        } else {
+          appbarElement.classList.add('open');
+          navdrawerContainer.classList.add('open');
+        }
+    }, true);
+</script>
+
+</html>
