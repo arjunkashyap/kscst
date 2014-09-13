3890990cfb591f6663cf2220bafe029cb73613f3
Arjuna <arjun.kashyap@yahoo.co.in>
2014-09-13 15:41:27 +0530

    Commit No. 8 - index file bug fix

--- index.php
+++ index.php
@@ -85,21 +85,4 @@
             <i class="fa fa-envelope-o" title="Email"></i> office@kscst.iisc.ernet.in
         </p>
     </footer>
-<script>
-    var navdrawerContainer = document.querySelector('.navdrawer-container');
-    var appbarElement = document.querySelector('.app-bar');
-    var menuBtn = document.querySelector('.menu');
-
-    menuBtn.addEventListener('click', function() {
-        var isOpen = navdrawerContainer.classList.contains('open');
-        if(isOpen) {
-            appbarElement.classList.remove('open');
-            navdrawerContainer.classList.remove('open');
-        } else {
-          appbarElement.classList.add('open');
-          navdrawerContainer.classList.add('open');
-        }
-    }, true);
-</script>
-
 </html>
